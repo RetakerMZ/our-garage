8291750768498c9d02fd1a01b966f4e4742f7757
Widiantara007 <Madewidiantara007@gmail.com>
2022-05-24 15:11:11 +0800

    Landing Page

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,273 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>OurGarage.Id</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+        <link rel="stylesheet" href="css/jquery.timepicker.css">
+        <!-- owl-carousel -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <!-- aos css -->
+        <link rel="stylesheet" href="css/aos.css">
+        <!-- ionicons -->
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <!-- flaticon -->
+        <link rel="stylesheet" href="css/flaticon.css">
+        <link rel="stylesheet" href="css/icomoon.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+<body>        
+<!-- navbar -->
+@include('layout.navbar')
+<!-- END nav -->
+        
+        <div class="headcover" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvU4_F4KBJXw2ACEovdr0zXW6Pwp4idUzhg&usqp=CAU');" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
+            <div class="col-lg-8 ftco-animate">
+                <div class="text w-100 text-center mb-md-5 pb-md-5">
+                    <h1 class="mb-4">Lorem &amp; Lorem ipsum dolor sit.</h1>
+                    <p style="font-size: 18px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, facere?</p>
+                        <div class="text">
+                            <p>Easy Way To Rent Anything</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <section class="ftco-section ftco-no-pt bg-light">
+            <div class="container">
+                <div class="row justify-content-center">
+            <div class="col-md-12 heading-section text-center ftco-animate mt-5">
+                <span class="subheading">OUR ITEMS</span>
+                <h2 class="mb-2">OUR ITEMS</h2>
+            </div>
+            </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="carousel-car owl-carousel">
+                            <div class="item">
+                                <div class="car-wrap rounded ftco-animate">
+                                    <div class="img rounded d-flex align-items-end" style="background-image: url(images/car-1.jpg);">
+                                    </div>
+                                    <div class="text">
+                                        <h2 class="mb-0"><a href="#">Lorem, ipsum dolor.</a></h2>
+                                        <div class="d-flex mb-3">
+                                            <span class="cat">Lorem, ipsum dolor.</span>
+                                            <p class="price ml-auto">lorem <span>/lorem</span></p>
+                                        </div>
+                                        <p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Lorem, ipsum.</a> <a href="#" class="btn btn-secondary py-2 ml-1">Details</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="car-wrap rounded ftco-animate">
+                                    <div class="img rounded d-flex align-items-end" style="background-image: url(images/car-1.jpg);">
+                                    </div>
+                                    <div class="text">
+                                        <h2 class="mb-0"><a href="#">Lorem, ipsum dolor.</a></h2>
+                                        <div class="d-flex mb-3">
+                                            <span class="cat">Lorem, ipsum dolor.</span>
+                                            <p class="price ml-auto">lorem <span>/lorem</span></p>
+                                        </div>
+                                        <p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Lorem, ipsum.</a> <a href="#" class="btn btn-secondary py-2 ml-1">Details</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="car-wrap rounded ftco-animate">
+                                    <div class="img rounded d-flex align-items-end" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvU4_F4KBJXw2ACEovdr0zXW6Pwp4idUzhg&usqp=CAU');">
+                                    </div>
+                                    <div class="text">
+                                        <h2 class="mb-0"><a href="#">Lorem, ipsum dolor.</a></h2>
+                                        <div class="d-flex mb-3">
+                                            <span class="cat">Lorem, ipsum dolor.</span>
+                                            <p class="price ml-auto">lorem <span>/lorem</span></p>
+                                        </div>
+                                        <p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Lorem, ipsum.</a> <a href="#" class="btn btn-secondary py-2 ml-1">Details</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="ftco-section ftco-about">
+                <div class="container">
+                    <div class="row no-gutters">
+                        <div class="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/about.jpg);">
+                        </div>
+                        <div class="col-md-6 wrap-about ftco-animate">
+                <div class="heading-section heading-section-white pl-md-5">
+                    <span class="subheading">About us</span>
+                    <h2 class="mb-4">Welcome to Carbook</h2>
+
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p><a href="#" class="btn btn-primary py-3 px-4">Search Vehicle</a></p>
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="ftco-section">
+                <div class="container">
+                    <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+                <span class="subheading">Services</span>
+                <h2 class="mb-3">Our Latest Services</h2>
+            </div>
+            </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="services services-2 w-100 text-center">
+                    <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-wedding-car"></span></div>
+                    <div class="text w-100">
+                    <h3 class="heading mb-2">Wedding Ceremony</h3>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                </div>
+                </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="services services-2 w-100 text-center">
+                    <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-transportation"></span></div>
+                    <div class="text w-100">
+                    <h3 class="heading mb-2">City Transfer</h3>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                </div>
+                </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="services services-2 w-100 text-center">
+                    <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-car"></span></div>
+                    <div class="text w-100">
+                    <h3 class="heading mb-2">Airport Transfer</h3>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                </div>
+                </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="services services-2 w-100 text-center">
+                    <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-transportation"></span></div>
+                    <div class="text w-100">
+                    <h3 class="heading mb-2">Whole City Tour</h3>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                </div>
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        <section class="ftco-section testimony-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+                <span class="subheading">Testimonial</span>
+                <h2 class="mb-3">Happy Clients</h2>
+            </div>
+            </div>
+            <div class="row ftco-animate">
+            <div class="col-md-12">
+                <div class="carousel-testimony owl-carousel ftco-owl">
+                <div class="item">
+                    <div class="testimony-wrap rounded text-center py-4 pb-5">
+                    <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                    </div>
+                    <div class="text pt-4">
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <p class="name">Roger Scott</p>
+                        <span class="position">Marketing Manager</span>
+                    </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimony-wrap rounded text-center py-4 pb-5">
+                    <div class="user-img mb-2" style="background-image: url(images/person_2.jpg)">
+                    </div>
+                    <div class="text pt-4">
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <p class="name">Roger Scott</p>
+                        <span class="position">Interface Designer</span>
+                    </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimony-wrap rounded text-center py-4 pb-5">
+                    <div class="user-img mb-2" style="background-image: url(images/person_3.jpg)">
+                    </div>
+                    <div class="text pt-4">
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <p class="name">Roger Scott</p>
+                        <span class="position">UI Designer</span>
+                    </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimony-wrap rounded text-center py-4 pb-5">
+                    <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                    </div>
+                    <div class="text pt-4">
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <p class="name">Roger Scott</p>
+                        <span class="position">Web Developer</span>
+                    </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimony-wrap rounded text-center py-4 pb-5">
+                    <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                    </div>
+                    <div class="text pt-4">
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <p class="name">Roger Scott</p>
+                        <span class="position">System Analyst</span>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>	
+<!-- footer         -->
+@include('layout.footer')
+<!-- end-footer -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+</body>
+</html>
