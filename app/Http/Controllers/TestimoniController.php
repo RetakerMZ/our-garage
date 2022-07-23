6239fade981d140f2b9df18f4549b56173e18987
Widiantara007 <Madewidiantara007@gmail.com>
2022-07-23 14:26:38 +0800

    testimoni

--- app/Http/Controllers/TestimoniController.php
+++ app/Http/Controllers/TestimoniController.php
@@ -2,9 +2,115 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimoni;
+use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class TestimoniController extends Controller
 {
-    //
+    public function index()
+    {
+        $testimonis = Testimoni::all();
+        return view('admin.testimoni.index',compact('testimonis'));
+    }
+
+    public function create()
+    {
+        return view('admin.testimoni.create');
+    }
+
+    public function insert(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string',
+            'gambar' => 'required|file|image',
+            'caption' => 'nullable|string',
+        ]);
+
+        $fileName = date("Y-m-d-His") . '_' . $request->file('image')->getClientOriginalName();
+
+        $image = $request->file('image')
+            ->storeAs('public/images/testimonis/', $fileName);
+
+        $store = testimoni::insert([
+            'nama' => 'required|string',
+            'gambar' => 'required|file|image',
+            'caption' => 'nullable|string',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        if($store){
+            Toastr::success('Data berhasil ditambahkan','Berhasil!');
+            return redirect()->route('testimoni.index');
+        }else{
+            Toastr::error('Data gagal ditambahkan, coba lagi','Gagal!');
+            return redirect()->back();
+        }
+
+    }
+    public function show(testimoni $testimoni)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        $testimoni = Testimoni::find($id);
+        return view('admin.testimoni.edit',compact('testimoni'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string',
+            'gambar' => 'required|file|image',
+            'caption' => 'nullable|string',
+        ]);
+        if ($request->hasFile('image')) {
+            $existingImage = Testimoni::find($id)->image;
+            Storage::delete('public/images/testimonis/' . $existingImage);
+
+
+            $fileName = date("Y-m-d-His") . '_' . $request->file('image')->getClientOriginalName();
+            $image = $request->file('image')
+                ->storeAs('public/images/testimonis/', $fileName);
+
+            $image = Testimoni::find($id)->update([
+                'image' => $fileName,
+            ]);
+        }
+        $testimoni = Testimoni::find($id);
+        $update = $testimoni->update([
+            'title' => $request->title,
+            'link' => $request->link,
+            'description' => $request->description,
+            'updated_at' => Carbon::now(),
+        ]);
+        if($update){
+            Toastr::success('Data berhasil diedit','Berhasil!');
+            return redirect()->route('testimoni.index');
+        }else{
+            Toastr::error('Data gagal diedit, coba lagi','Gagal!');
+            return redirect()->back();
+        }
+
+    }
+    public function destroy($id)
+    {
+        $testimoni = Testimoni::find($id);
+        $existingImage = Testimoni::find($id)->image;
+        Storage::delete('public/images/testimonis/' . $existingImage);
+        $delete = $testimoni->delete();
+        if($delete){
+            Toastr::success('Testimoni berhasil dihapus','Berhasil!');
+            return redirect()->route('testimoni.index');
+        }else{
+            Toastr::error('Testimoni gagal dihapus, coba lagi','Gagal!');
+            return redirect()->back();
+        }
+    }
+
 }
