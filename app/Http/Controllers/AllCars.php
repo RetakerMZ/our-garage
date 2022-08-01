a77bda1d344eb04b5c0f4c8659687498d1a4e443
RetakerMZ <reiki290@gmail.com>
2022-08-01 17:12:56 +0800

    s

--- app/Http/Controllers/AllCars.php
+++ app/Http/Controllers/AllCars.php
@@ -22,8 +22,8 @@ class AllCars extends Controller
             if(count($request->tipe_mobil) != count($types)){
                 $allcars = $allcars->whereIn('tipe_mobil',$request->tipe_mobil);
             }
-            dd($request->tipe_mobil);
         }
+        dd($request->tipe_mobil);
         return view('car.all_car',compact('allcars'));
     }
 
