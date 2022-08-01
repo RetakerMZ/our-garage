9dfe67db382bf2cce1ef4f1ad7c035bedc86f8e6
RetakerMZ <reiki290@gmail.com>
2022-08-01 15:20:06 +0800

    di

--- resources/views/admin/car/car/index.blade.php
+++ resources/views/admin/car/car/index.blade.php
@@ -66,9 +66,9 @@
                       </td>
                       <td>
                         <?php if ($row->rent == 'yes') : ?>
-                            <a id="btn-{{$row->id}}" class="btn btn-warning" onclick="window.location'{{ route('car.car.rented', $row->id) }}'">Rented</a>
+                            <button id="btn-{{$row->id}}" class="btn btn-warning" onclick="window.location'{{ route('car.car.rented', $row->id) }}'">Rented</button>
                         <?php else : ?>
-                            <a id="btn-{{$row->id}}" class="btn btn-warning" onclick="window.location'{{ route('car.car.available', $row->id) }}'">Available</a>
+                            <button id="btn-{{$row->id}}" class="btn btn-warning" onclick="window.location'{{ route('car.car.available', $row->id) }}'">Available</button>
                         <?php endif; ?>
 
 
