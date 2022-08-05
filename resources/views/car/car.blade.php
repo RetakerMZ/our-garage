795c5a45579fb5b2f317c1a32e40c3bf4ab74e52
RetakerMZ <reiki290@gmail.com>
2022-08-05 11:19:10 +0800

    s

--- resources/views/car/car.blade.php
+++ resources/views/car/car.blade.php
@@ -23,6 +23,7 @@
           <div class="col-lg-2 col-md-4 col-4 car__">
             <div class="category-car">
               <div class="categoryimg">
+                <dd>{{$tipe->tipe_mobil}}</dd>
                 <a href="{{ route('allcar', $tipe->tipe_mobil) }}"><img src="{{asset('storage/images/tipe_mobils/'.$tipe->gambar)}}" alt=""></a>
                 <p>{{$tipe->tipe_mobil}}</p>
               </div>
